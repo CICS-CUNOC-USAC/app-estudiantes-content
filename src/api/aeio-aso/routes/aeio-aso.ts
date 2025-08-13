b56b53709ecff1c073ec3f2367da872650814783
Roberto Bautista <44622828+rrobertobt@users.noreply.github.com>
2025-08-13 17:28:27 -0600

    feat: add AEIO, CICS, and Comunicado content types with controllers, routes, and services

---
+++ src/api/aeio-aso/routes/aeio-aso.ts
@@ -0,0 +1,7 @@
+/**
+ * aeio-aso router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::aeio-aso.aeio-aso');
---
+++ src/api/cics-aso/services/cics-aso.ts
@@ -0,0 +1,7 @@
+/**
+ * cics-aso service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::cics-aso.cics-aso');
